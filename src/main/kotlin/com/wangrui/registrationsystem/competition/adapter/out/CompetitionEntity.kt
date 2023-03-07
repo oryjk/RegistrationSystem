7a28ae31b159b1b6d1d8a5e7c7ced8f32ca21209
WangRui <wangrui@betalpha.com>
2023-03-08 00:02:33 +0800

    feat(competition): add competition logic
    #8

---
+++ src/main/kotlin/com/wangrui/registrationsystem/competition/adapter/out/CompetitionEntity.kt
@@ -0,0 +1,50 @@
+package com.wangrui.registrationsystem.competition.adapter.out
+
+import com.wangrui.registrationsystem.competition.domain.Competition
+import com.wangrui.registrationsystem.competition.domain.CompetitionId
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+/**
+ * Created on 2023/3/7 23:16.
+ * @author wangrui
+ * @since 0.0.1
+ */
+@Entity
+@Table(name = "competition")
+open class CompetitionEntity(
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE) @Column(
+        name = "id", nullable = false
+    ) val id: CompetitionId?,
+    private val name: String?,
+    private val gameType: Competition.GameType?,
+    private val gameDate: LocalDateTime?,
+    private val venue: String?,
+    private val contacts: String?,
+    private val competitionStatement: String?,
+    private val applicantsLimit: Int?,
+    val createTime: LocalDateTime?
+) {
+
+
+    fun toCompetition(): Competition {
+        return Competition(id, name, gameType, gameDate, venue, contacts, competitionStatement, applicantsLimit)
+    }
+
+    companion object {
+        fun toCompetitionEntity(competition: Competition): CompetitionEntity {
+            return CompetitionEntity(
+                competition.id,
+                competition.name,
+                competition.gameType,
+                competition.gameDate,
+                competition.venue,
+                competition.contacts,
+                competition.competitionStatement,
+                competition.applicantsLimit,
+                LocalDateTime.now()
+            )
+        }
+    }
+
+}
