67edd77ad7ce46904e895dda63be87078dd7136e
WangRui <wangrui@betalpha.com>
2023-10-11 08:31:15 +0800

    add tabbar

--- myApp/src/app.config.ts
+++ myApp/src/app.config.ts
@@ -1,11 +1,27 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index'
-  ],
-  window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
+    pages: [
+        'pages/index/index'
+    ],
+    window: {
+        backgroundTextStyle: 'light',
+        navigationBarBackgroundColor: '#fff',
+        navigationBarTitleText: 'WeChat',
+        navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+        custom: true,
+        color: '#000000',
+        selectedColor: '#000000',
+        backgroundColor: '#000000',
+        list: [
+            {
+                pagePath: 'pages/component/index',
+                text: '组件',
+            },
+            {
+                pagePath: 'pages/API/index',
+                text: '接口',
+            },
+        ],
+    },
 })
---
+++ myApp/src/pages/component/index.config.js
@@ -0,0 +1,4 @@
+export default {
+    navigationBarTitleText: '组件页',
+    usingComponents: {},
+}
