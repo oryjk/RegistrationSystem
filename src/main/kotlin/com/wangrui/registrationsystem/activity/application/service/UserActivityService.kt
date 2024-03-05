9dc2ab47f0228980b5d1a524850eaef207457f85
carlwang <wangrui@betalpha.com>
2024-03-05 22:27:47 +0800

    feat(userActivity): add UserActivityEntity and repository

--- src/main/kotlin/com/wangrui/registrationsystem/activity/application/service/UserActivityService.kt
+++ src/main/kotlin/com/wangrui/registrationsystem/activity/application/service/UserActivityService.kt
@@ -1,12 +1,13 @@
 package com.wangrui.registrationsystem.activity.application.service
 
 import com.wangrui.registrationsystem.activity.application.port.input.UserActivityUseCase
+import com.wangrui.registrationsystem.activity.application.port.output.UserActivityDao
 import com.wangrui.registrationsystem.activity.domain.UserActivity
 import org.springframework.stereotype.Service
 
 @Service
-class UserActivityService: UserActivityUseCase {
+class UserActivityService(val userActivityDao: UserActivityDao) : UserActivityUseCase {
     override fun register(userActivity: UserActivity) {
-        TODO("Not yet implemented")
+        userActivityDao.save(userActivity)
     }
 }
